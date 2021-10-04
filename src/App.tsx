237128877a1d25b892e15a8809f1504d7788dc30
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-10-04 17:20:45 -0600

    added increment and decrement methods utilizing useState

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const { useState } = React;
+
 function App() {
+
+  const [counter, setCounter] = useState(0);
+  const [command, setCommand] = useState('');
+
+  const increment = () => {
+    setCounter(counter + 1);
+
+  }
+
+  const decrement = () => {
+    setCounter(counter - 1);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          INTERVIEW PREP
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      {/* COUNTER */}
+
+      <p>{counter}</p>
+      <button onClick={() => decrement()}>Decrement</button>
+      <button onClick={() => increment()}>Increment</button>
+
+
+
     </div>
   );
 }
