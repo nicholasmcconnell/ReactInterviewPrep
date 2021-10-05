fd7dcb309324817f44a8169862a19cead7c391db
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-10-04 18:16:32 -0600

    removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ function App() {
       }
     }
     jsonGet();
-  }, [counter])
+  }, [])
 
   return (
     <div className="App">
@@ -46,7 +46,7 @@ function App() {
       {/* ------------ COUNTER ------------ */}
 
       <p>{counter}</p>
-      <button onClick={() => decrement()}>Decrement</button>
+      <button onClick={() => [decrement()]}>Decrement</button>
       <button onClick={() => increment()}>Increment</button>
 
       {/* ------------ AXIOS CALL -------------- */}
