2e5f335750b92add827c642d6169ee226dd4e8b1
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-10-06 15:53:08 -0600

    completed timer, display user, and appened users challange

--- src/App.tsx
+++ src/App.tsx
@@ -7,34 +7,63 @@ const { useState, useEffect } = React;
 
 function App() {
 
-  const [counter, setCounter] = useState(0);
-  const [jsonRes, setJsonRes] = useState('');
+  const [randomUserDataJSON, setRandomUserData] = useState<any>([]);
+  const [nextPageNumber, setNextPageNumber] = useState<any>(1);
+  const [jsonRes, setJsonRes] = useState();
+  const [users, setUsers] = useState([]);
 
-  const increment = () => {
-    setCounter(counter + 1);
+  const fetchRandomData = async (pageNumber: number) => {
+    try {
+      let { data } = await axios.get(`https://randomuser.me/api?page=${pageNumber}`);
+      let { results } = data;
 
+      setRandomUserData(results);
+      setUsers([...users, ...results]);
+
+    } catch (err) {
+      console.error(err);
+    }
   }
-
-  const decrement = () => {
-    setCounter(counter - 1);
-  }
-
 
 
   useEffect(() => {
-    const jsonGet = async () => {
-      try {
-        let { data: { results } } = await axios.get('https://randomuser.me/api');
-        // console.log(data);
-        setJsonRes(JSON.stringify(results, null, 10));
-
-      } catch (err) {
-        console.error(err);
-      }
-    }
-    jsonGet();
+    fetchRandomData(nextPageNumber);
   }, [])
 
+  interface UserName {
+    first: string,
+    last: string,
+    title: string
+  }
+
+  interface UserPicture {
+    large: string,
+  }
+
+  interface UserInfo {
+    userInfo: UserName,
+    picture: UserPicture,
+  }
+
+  const getFullUserName = (randomUser: any) => {
+    const { name: { first, last } } = randomUser;
+    return `${first} ${last}`;
+  }
+
+  const getUserImage = (randomUser: any): string => {
+    const { picture: { large } } = randomUser;
+    return large;
+  }
+
+  const addUsers = async () => {
+    try {
+      fetchRandomData(nextPageNumber);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // const getUserImage = ()
   return (
     <div className="App">
       <header className="App-header">
@@ -45,15 +74,37 @@ function App() {
 
       {/* ------------ COUNTER ------------ */}
 
-      <p>{counter}</p>
+      {/* <p>{counter}</p>
       <button onClick={() => [decrement()]}>Decrement</button>
-      <button onClick={() => increment()}>Increment</button>
+      <button onClick={() => increment()}>Increment</button> */}
 
       {/* ------------ AXIOS CALL -------------- */}
 
-      <p>
-        {jsonRes}
-      </p>
+      {/* ------------ RENDER NAME AND PIC TO SCREEN -------------- */}
+
+      <button
+        onClick={() => addUsers()}
+      >
+        Fetch Next User
+      </button>
+      <div>
+        {users.map((randomUser: any, idx: number) => {
+          // let { name: { first, last } } = randomUser;
+          let { picture: { large } } = randomUser;
+          return (
+            <div>
+              <h2
+                key={idx}
+              >
+                {getFullUserName(randomUser)}</h2>
+              <img src={large} alt='User'></img>
+            </div>
+          )
+        })}
+      </div>
+      {/* ------------ APPEND USERS TO SCREEN -------------- */}
+
+
 
     </div>
   );
